137b15030b097176e6ba600fd55e1a287fd08882
NicoCraft31 <nicolasgraciacotescraft31@gmail.com>
2023-04-04 10:55:08 +0200

    Added comment in IShitableInformation

--- src/main/java/me/nicocraft31/refinery/common/item/IShiftableInformation.java
+++ src/main/java/me/nicocraft31/refinery/common/item/IShiftableInformation.java
@@ -2,6 +2,13 @@ package me.nicocraft31.refinery.common.item;
 
 import me.nicocraft31.refinery.RefineryCraft;
 
+/**
+ * 
+ * ANOTHER time (3 times now) I stealed this from some poor random dude.
+ * This time is from Galacticraft.
+ *
+ */
+
 public interface IShiftableInformation {
 	public String getShiftDescription();
 	public boolean hasSpace();
