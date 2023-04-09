0bcfe026e373e4a42806c1ca9e7d12cfe1042135
NicoCraft31 <nicolasgraciacotescraft31@gmail.com>
2023-04-09 22:16:30 +0200

    Removed right-click event from ClientUtil from the Item class itself.

--- src/main/java/me/nicocraft31/refinery/common/item/ItemLinker.java
+++ src/main/java/me/nicocraft31/refinery/common/item/ItemLinker.java
@@ -3,13 +3,22 @@ package me.nicocraft31.refinery.common.item;
 import java.util.List;
 
 import me.nicocraft31.refinery.client.ClientUtil;
+import me.nicocraft31.refinery.common.block.BlockWirelessTransciver;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
+import net.minecraftforge.energy.CapabilityEnergy;
 
 public class ItemLinker extends ItemBasic {
 	private BlockPos pos = null;
@@ -30,6 +39,46 @@ public class ItemLinker extends ItemBasic {
 			
 			pos = new BlockPos(x, y, z);
 		}
+	}
+	
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+		ActionResult<ItemStack> result = new ActionResult<ItemStack>(EnumActionResult.FAIL, playerIn.getHeldItem(handIn));
+
+		if(!playerIn.isSneaking())
+			return result; 
+		
+		ItemStack stack = playerIn.getHeldItem(handIn);
+		World world = worldIn;
+		if(world.isRemote)
+			return result;
+		RayTraceResult hover_object = Minecraft.getMinecraft().objectMouseOver;
+		if(hover_object == null)
+			return result;
+		
+		BlockPos pos = hover_object.getBlockPos();
+		IBlockState state = world.getBlockState(pos);
+		Block block = state.getBlock();
+		if(block instanceof BlockWirelessTransciver)
+			return result;
+		
+		if(!block.hasTileEntity(state))
+			return result;
+		
+		int count = 0;
+		for(EnumFacing f : EnumFacing.values())
+		{
+			if(world.getTileEntity(pos).hasCapability(CapabilityEnergy.ENERGY, f))
+				count++;
+		}
+		if(count <= 0)
+			return result;
+		
+		ItemLinker vinculer = (ItemLinker) stack.getItem();
+		vinculer.setBlockPos(pos);
+		vinculer.onShiftRightClick(stack, world, playerIn, handIn);
+		
+		return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, stack);
 	}
 	
 	@Override
